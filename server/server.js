fb3f34b9fa94b54b75323581b484c99385d96d0e
MarcZaharescu <=zaharescumarc10@gmail.com>
2018-09-05 13:36:37 +0100

    Finished the server. Added basic index.html

--- server/server.js
+++ server/server.js
@@ -7,8 +7,63 @@ function requestHandler(req,res) {
 
   let reqResource = path.join(
     __dirname,
-    '..public',
+    '../public',
     url.parse(req.url).pathname
   );
 
+  fs.exists(reqResource, (exists) => {
+
+    if(!exists){
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.write("404 Not Found\n");
+      res.end();
+      return;
+    }
+
+    if(fs.statSync(reqResource).isDirectory()){
+      reqResource +='index.html';
+    }
+
+    fs.readFile(
+      reqResource,
+      "binary",
+      function(err, file){
+
+        if(err){
+          res.writeHead(500, {"content-Type": "text/plain"});
+          res.write(err + "\n");
+          res.end();
+          return;
+        }
+
+        const contentTypeByExtension = {
+          ".html":  "text/html",
+          ".css":   "text/css",
+          ".js":    "text/javascript"
+        };
+
+        const headers = {};
+
+        const contentType = contentTypeByExtension[
+          path.extname(reqResource)
+        ];
+
+        if(contentType){
+          headers["Content-Type"] = contentType;
+        }
+
+        res.writeHead(200, headers);
+        res.write(file, "binary");
+        res.end();
+
+      });
+    });
+
 }
+
+const server = http.createServer(requestHandler);
+const portNumber = 3030;
+
+server.listen(portNumber, function(){
+  console.log(`Server is listening to ${portNumber}`);
+})
